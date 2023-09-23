11b882401f9fc4c4590b82193ae79282a421d73e
hernan findlay <hernanfindlay@hotmail.com.ar>
2023-09-23 18:44:33 -0300

    primer-entrega

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+let paciente = prompt ("Ingrese nombre y apellido")
+let dni = parseIntprompt ("Escriba su DNI...")
+
+
+if (paciente != ""){
+    alert("Bienvenido " + paciente)
+
+}else{
+    alert("POR FAVOR COMPLETE LOS DATOS")
+}
+
+
+
+
+if(dni != Number){
+    alert("NUMERO EQUIVOCADO")
+}
