ac2940240e09c0a864ef4ae7d934f3358f1ad8a9
hernan findlay <hernanfindlay@hotmail.com.ar>
2023-09-23 20:32:05 -0300

    intentos

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 
 
 let paciente = prompt ("Ingrese nombre y apellido")
-let dni = parseIntprompt ("Escriba su DNI...")
+let dni = prompt ("Escriba su DNI...")
+let turnos = 0
 
 
-if (paciente != ""){
-    alert("Bienvenido " + paciente)
-
+if ((paciente != "") && (dni != "")){
+    
+    
+    alert("Bienvenido " + paciente +" " + dni+ " " + resultado )
 }else{
     alert("POR FAVOR COMPLETE LOS DATOS")
 }
 
-
-
-
-if(dni != Number){
-    alert("NUMERO EQUIVOCADO")
+for (turnos; turnos = 10; turnos++ ){
+    let resultado = turnos * 1;
+    if (resultado >= 10){
+        alert("No hay mas turnos")
+    }
+    
 }
+
+
+
